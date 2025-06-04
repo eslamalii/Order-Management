27d834ded4f5a4775ab7c04becf9e9bee054f701
eslamalii <preslamali@hotmail.com>
2025-06-04 16:33:13 +0300

    feat: set up Swagger UI for API documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import 'reflect-metadata'
 import express, { Application, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/database'
-
+import authRoutes from './routes/authRoutes'
+import { errorHandler } from './utils/errorHandler'
+import swaggerUi from 'swagger-ui-express'
+import { swaggerSpec } from './docs/swagger'
 dotenv.config()
 
 const app: Application = express()
@@ -9,13 +13,33 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: 'Order Management API Documentation',
+    swaggerOptions: {
+      persistAuthorization: true,
+      displayRequestDuration: true,
+      filter: true,
+      showExtensions: true,
+      showCommonExtensions: true,
+    },
+  })
+)
+
 app.get('/', (_req: Request, res: Response) => {
   res.json({ message: '🎉 API is up and running' })
 })
+
+app.use('/api/auth', authRoutes)
+app.use(errorHandler)
 ;(async () => {
   await connectDB()
 
   app.listen(PORT, () => {
     console.log(`🚀 Server listening on http://localhost:${PORT}`)
+    console.log(`📚 API Docs: http://localhost:${PORT}/api-docs`)
   })
 })()
